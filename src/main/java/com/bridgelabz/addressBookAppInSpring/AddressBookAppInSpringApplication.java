cc4d4051d3eb4c29a3bdb92e2f93e9ac9aefdaab
mukulBillore <mukulbillore88@gmail.com>
2022-02-20 23:45:36 +0530

    added the project layers

---
+++ src/main/java/com/bridgelabz/addressBookAppInSpring/AddressBookAppInSpringApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.addressBookAppInSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookAppInSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressBookAppInSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/addressBookAppInSpring/AddressBookAppInSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.addressBookAppInSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookAppInSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
